97dc9c8929119600f20256cea419d7c280f9ec36
SwissalpS <Luke@SwissalpS.ws>
2020-02-10 03:21:44 +0100

    minimal to clear spawn from time to time

--- init.lua
+++ init.lua
@@ -10,16 +10,8 @@ distributed without any warranty.
 afkkick = {}
 
 afkkick.maxInactiveTime = tonumber(minetest.settings:get("afkkick.max_inactive_time") or "1800")
--- TODO: discuss if these may be useful as server settings
-afkkick.checkInterval = 2
-afkkick.warnTime = 60
+afkkick.checkInterval = 120
 afkkick.radius = 13
-
-afkkick.exemptXP = 100000
-afkkick.hasXPredo = minetest.get_modpath('xp_redo')
-
--- TODO: make these translatable
-afkkick.kickWarning = "Warning, you have %d seconds to move away from spawn island or be kicked"
 afkkick.kickMessage = "Kicked for loitering at spawn"
 
 -- one time calculation of kickarea
@@ -60,21 +52,13 @@ minetest.register_globalstep(function()
 
 	local playerPos, playerName
 	local isInX, isInY, isInZ, isAtSpawn
-	local kickTime, warnTime
-	local xp
+	local kickTime
 
 	-- Loop through each player that is online
 	for _, player in ipairs(minetest.get_connected_players()) do
 		playerName = player:get_player_name()
 		-- only bother if is a real player
 		if 0 < #playerName then
-			if afkkick.hasXPredo then
-				xp = xp_redo.get_xp(playerName)
-			else
-				xp = -1
-			end
-			-- ignore players that have enough XP
-			if xp < afkkick.exemptXP then
 				--Check if this player is near spawn
 				playerPos = player:get_pos()
 				isInX = (playerPos.x > afkkick.posB.x) and (playerPos.x < afkkick.posA.x)
@@ -83,17 +67,12 @@ minetest.register_globalstep(function()
 				isAtSpawn = isInX and isInY and isInZ
 				if isAtSpawn then
 					kickTime = afkkick.players[playerName].kickAt
-					warnTime = kickTime - afkkick.warnTime
 					if 0 == kickTime then
 						-- first time we check and player is at spawn
 						players[playerName].kickAt = currentTime + afkkick.maxInactiveTime
 					elseif kickTime < currentTime then
 						-- player has been here long enough, kick
 						minetest.kick_player(playerName, afkkick.kickMessage)
-					elseif warnTime < currentTime then
-						minetest.chat_send_player(playerName,
-							minetest.colorize("#FF8C00",
-								afkkick.kickWarning:format(kickTime - currentTime)))
 					end
 				else
 					-- player is out of kick location
